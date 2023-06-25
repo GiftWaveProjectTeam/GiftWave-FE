3a51deadef02098f0124ba8734c809892cbd7ad3
Inseo Yang <y.1nseooo@gmail.com>
2023-06-25 14:48:03 +0900

    [RENAME]  :  ✏️ app router 추가 및 레이아웃 파일 재설정

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 
 import "@testing-library/jest-dom";
-import Home from "@/src/pages/index";
+import Home from "@/src/app/page";
 
 describe("<Home />", () => {
   it("renders a heading", () => {
