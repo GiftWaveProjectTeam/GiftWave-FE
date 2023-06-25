2aaea76edffb6cea5f6acf3345841ff53d12b3d4
ChanwooPark <chanw9503@gmail.com>
2023-06-25 22:33:21 +0900

    [FEAT] : ⚙️  Signup Page add

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,5 +1,12 @@
-import React from "react";
+import * as styles from "../styles/signup.css";
+import SignupForm from "../components/SignupForm/SignupForm";
 
 export default function SignupPage() {
-  return <div>SignupPage</div>;
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.container}>
+        <SignupForm />
+      </div>
+    </div>
+  );
 }
