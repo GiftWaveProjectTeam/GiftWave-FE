e4a1469c30171b8cc8275ea99097f60ccf1257e5
Inseo Yang <y.1nseooo@gmail.com>
2023-06-25 18:06:22 +0900

    [PACK] : 🎁 server 관련 라이브러리 설치

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,13 +4,29 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import React, { PropsWithChildren } from "react";
 
-export default function Providers({ children }: PropsWithChildren) {
-  const [queryClient] = React.useState(() => new QueryClient());
+type Props = {
+  children: React.ReactNode;
+};
+
+function Providers({ children }: Props) {
+  const [client] = React.useState(
+    new QueryClient({
+      defaultOptions: {
+        // react-query 전역 설정
+        queries: {
+          refetchOnWindowFocus: false,
+          retry: false,
+        },
+      },
+    })
+  );
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools initialIsOpen={false} />
+    <QueryClientProvider client={client}>
       {children}
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
+
+export default Providers;
