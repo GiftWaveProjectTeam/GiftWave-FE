8ed5f658a341b37cdc1c50c38a617de339a338ca
ChanwooPark <chanw9503@gmail.com>
2023-06-28 16:26:30 +0900

    [FEAT] : ⚙️ Signup페이지에서 쓰일 Timer 기능 생성 - 기능적부분은 hook으로 분리를 했고, 핸드폰 인증 시간에 대한 timer기능입니다.

---
+++ src/app/hooks/useInterval.ts
@@ -0,0 +1,27 @@
+import { useEffect, useRef } from "react";
+
+type Callback = () => void;
+
+export function useInterval(
+  callback: Callback,
+  delay: number,
+  trigger?: boolean
+) {
+  const savedCallback = useRef<Callback>();
+
+  //Remember the latest callback
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback, trigger]);
+
+  useEffect(() => {
+    function tick() {
+      savedCallback.current !== undefined && savedCallback.current();
+    }
+
+    if (delay !== null && trigger !== false) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay, trigger]);
+}
