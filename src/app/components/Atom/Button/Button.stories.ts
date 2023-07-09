83b0cf767822d2b2af24f0cc9bcbb2cbfb489ab2
Inseo Yang <y.1nseooo@gmail.com>
2023-07-10 00:23:58 +0900

    [FEAT] : ⚙️ button component test (진행중)
    Co-authored-by: chanw9503 <chanw9503@users.noreply.github.com>

--- src/app/components/Atom/Button/Button.stories.ts
+++ src/app/components/Atom/Button/Button.stories.ts
@@ -7,11 +7,7 @@ const meta: Meta<typeof Button> = {
   title: "Atom/Button",
   component: Button,
   tags: ["autodocs"],
-  argTypes: {
-    backgroundColor: {
-      control: "color",
-    },
-  },
+  argTypes: {},
 };
 
 export default meta;
