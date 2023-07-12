b082c5d5bd4bdd33510f68171fb58f3724603dfb
ChanwooPark <chanw9503@gmail.com>
2023-07-12 21:20:44 +0900

    [RENAME]  : ✏️  컴포넌트 위치 변경으로 인한 수정

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,5 +1,5 @@
 import * as styles from "../styles/signup.css";
-import SignupForm from "../components/SignupForm/SignupForm";
+import SignupForm from "../components/Organism/SignupForm/SignupForm";
 
 export default function SignupPage() {
   return (
