db7df9c18536f1996a701609d01e62aa1a1213e1
Inseo Yang <y.1nseooo@gmail.com>
2023-07-12 10:33:42 +0900

    [FIX] : 🔧 Atom - Input type 수정 #15 #16
    Co-authored-by: chanw9503 <chanw9503@users.noreply.github.com>

--- src/app/components/Atom/Input/Input.tsx
+++ src/app/components/Atom/Input/Input.tsx
@@ -2,11 +2,13 @@ import { ChangeEvent } from "react";
 import * as styles from "./Input.css";
 
 interface InputProps {
-  type?: string;
+  type?: "text" | "file" | "password" | "radio" | "select" | "checkbox";
   name?: string;
   className?: string;
   value?: string;
   onChangeHandler?: (e: ChangeEvent<HTMLInputElement>) => void;
+  disabled?: boolean;
+  checked?: boolean;
 }
 
 export default function Input({
@@ -15,9 +17,13 @@ export default function Input({
   className,
   value,
   onChangeHandler,
+  disabled,
+  checked,
 }: InputProps) {
   return (
     <input
+      disabled={disabled}
+      checked={checked}
       type={type}
       name={name}
       className={className}
