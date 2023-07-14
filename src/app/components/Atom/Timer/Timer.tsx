3dc2569977f2d8c5c51ac2f9218e0d5a211a015f
ChanwooPark <chanw9503@gmail.com>
2023-07-14 14:23:50 +0900

    [REFACTOR] : ♻️  경로 단축

--- src/app/components/Atom/Timer/Timer.tsx
+++ src/app/components/Atom/Timer/Timer.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from "react";
 import React from "react";
 import { TimerVariant } from "./Timer.css";
-import { useInterval } from "@/app/hooks/useInterval";
+import { useInterval } from "@hooks";
 
 interface TimerProps {
   maxTime: number;
