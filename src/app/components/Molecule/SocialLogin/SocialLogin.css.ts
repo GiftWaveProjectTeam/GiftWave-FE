ee85eb6315e2615135ee36e57d2438b6e301ee9a
ChanwooPark <chanw9503@gmail.com>
2023-07-14 14:25:36 +0900

    [FEAT] : ⚙️  Social Login Button 추가

---
+++ src/app/components/Molecule/SocialLogin/SocialLogin.css.ts
@@ -0,0 +1,11 @@
+import { style } from "@vanilla-extract/css";
+
+export const LoginBox = style({
+  width: "350px",
+  height: "200px",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  marginBottom: "3px",
+});
