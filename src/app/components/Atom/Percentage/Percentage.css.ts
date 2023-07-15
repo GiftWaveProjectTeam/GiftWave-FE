b9697ddc18c701b5c22cfd82c471be45541225bc
ChanwooPark <chanw9503@gmail.com>
2023-07-15 20:12:45 +0900

    [DESIGN] : 🎨 Percentage Bar style add

---
+++ src/app/components/Atom/Percentage/Percentage.css.ts
@@ -0,0 +1,14 @@
+import { style } from "@vanilla-extract/css";
+
+export const container = style({
+  width: "150px",
+  height: "10px",
+  backgroundColor: "#D9D9D9",
+  borderRadius: "50px",
+});
+
+export const percentbar = style({
+  height: "10px",
+  backgroundColor: "#767676",
+  borderRadius: "50px",
+});
