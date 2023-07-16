259e9c27a9368391622f865edf47a2187386816e
ChanwooPark <chanw9503@gmail.com>
2023-07-16 15:57:00 +0900

    [FEAT] : ⚙️ Custom 경로 추가

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -5,10 +5,15 @@ export { default as Input } from "@components/Atom/Input/Input";
 export { default as Label } from "@components/Atom/Label/Label";
 export { default as Portal } from "@components/Atom/Portal/Portal";
 export { default as Timer } from "@components/Atom/Timer/Timer";
+export { default as Text } from "@components/Atom/Text/Text";
+export { default as Percentage } from "@components/Atom/Percentage/Percentage";
 
 //molecules
 export { default as LabelInput } from "./Molecule/LabelInput/LabelInput";
 export { default as AuthInput } from "./Organism/SignupForm/AuthInput/AuthInput";
+export { default as Title } from "./Molecule/Title/Title";
+export { default as FundingCard } from "./Molecule/FundingCard/FundingCard";
+
 //Organism
 export { default as SignupForm } from "./Organism/SignupForm/SignupForm";
 export { default as Header } from "./Organism/Header/Header";
