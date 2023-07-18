cab4566a31ed57ef3ab34541d9918de4c0ad3f98
ChanwooPark <chanw9503@gmail.com>
2023-07-18 14:59:08 +0900

    [REFACTOR] : ♻️ Header Component 디자인에 맞게 변경

--- src/app/components/Organism/Header/Header.tsx
+++ src/app/components/Organism/Header/Header.tsx
@@ -1,10 +1,6 @@
 import React from "react";
-import {
-  ButtonBar,
-  MenuContainer,
-  gridItem,
-  navCotainer,
-} from "../../../styles/nav.css";
+import * as styles from "./Header.css";
+import GiftWaveLogo from "../../Atom/GiftWaveLogo/GiftWaveLogo";
 
 type User = {
   name?: string;
@@ -16,22 +12,27 @@ interface NavProps {
 
 export default function Header({ user }: NavProps) {
   return (
-    <div className={navCotainer}>
-      <div>GiftWave</div>
-      <div className={MenuContainer}>
-        <div className={gridItem}>메인 홈</div>
-        <div className={gridItem}>펀딩하기</div>
-        <div className={gridItem}>펀딩생성</div>
-        <div className={gridItem}>마이페이지</div>
+    <div className={styles.navCotainer}>
+      <div className={styles.logoBlock}>
+        <GiftWaveLogo />
       </div>
-      <div className={ButtonBar}>
+      <div className={styles.menuContainer}>
+        <div className={styles.gridItem}>메인 홈</div>
+        <div className={styles.gridItem}>펀딩하기</div>
+        <div className={styles.gridItem}>펀딩생성</div>
+        <div className={styles.gridItem}>마이페이지</div>
+      </div>
+      <div className={styles.buttonBar}>
         {user ? (
           <>
             <label>{user.name}</label>
             <button>로그아웃</button>
           </>
         ) : (
-          <button>로그인</button>
+          <>
+            <div className={styles.commentStyle}>로그인이 필요합니다.</div>
+            <button className={styles.buttonStyle}>로그인</button>
+          </>
         )}
       </div>
     </div>
