0bc5fb53f87b85852ef810080fb1b43fdf72b60f
1nxeo <y.1nseooo@gmail.com>
2023-07-20 13:29:08 +0900

    [FIX] : 🔧 FundInput molecule 생성을 위한 Text atom type 수정 #22

--- src/app/components/Atom/Text/Text.stories.ts
+++ src/app/components/Atom/Text/Text.stories.ts
@@ -5,7 +5,7 @@ import Text from "./Text";
 const meta: Meta<typeof Text> = {
   title: "Atom/Text",
   component: Text,
-  tags: ["autodocs"],
+  tags: ["autodocs"]
 };
 
 export default meta;
@@ -15,27 +15,34 @@ type Story = StoryObj<typeof Text>;
 export const Title: Story = {
   args: {
     children: "이것이 제목이다.",
-    type: "title",
-  },
+    type: "title"
+  }
 };
 
 export const SubTitle: Story = {
   args: {
     children: "이것이 부 제목이다.",
-    type: "subTitle",
-  },
+    type: "subTitle"
+  }
 };
 
 export const CardTitle: Story = {
   args: {
     children: "이것이 카드 제목이다.",
-    type: "cardTitle",
-  },
+    type: "cardTitle"
+  }
 };
 
 export const Amount: Story = {
   args: {
     children: "이것이 금액이다.",
-    type: "amount",
-  },
+    type: "amount"
+  }
+};
+
+export const InputTitle: Story = {
+  args: {
+    children: "이것은 라벨인척하는 텍스트다.",
+    type: "inputTitle"
+  }
 };
