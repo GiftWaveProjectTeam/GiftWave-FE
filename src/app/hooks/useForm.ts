656580c55ab35444bdc37475ce771af68df4e7a1
ChanwooPark <chanw9503@gmail.com>
2023-07-22 00:05:50 +0900

    [REFACTOR] : ♻️ 생년월일 정규식 추가

--- src/app/hooks/useForm.ts
+++ src/app/hooks/useForm.ts
@@ -16,10 +16,15 @@ const useForm = ({ initialValues, onSubmit, validate }: useFormProps) => {
     //비밀번호는 공백을 포함할 수 있다.
     if (name.includes("password") || name.includes("Password")) {
       setValues({ ...values, [name]: value });
+    } else if (name.includes("birthday") || name.includes("Birthday")) {
+      const newValue = value
+        .replace(/[^0-9]/g, "")
+        .replace(/^(\d{0,4})(\d{0,2})(\d{0,2})$/g, "$1-$2-$3")
+        .replace(/(\-{1,2})$/g, "");
+      setValues({ ...values, [name]: newValue.trim() });
     } else {
       setValues({ ...values, [name]: value.trim() });
     }
-
     const newValue = { ...values, [name]: value };
     const newErrors = validate ? validate(newValue) : {};
 
